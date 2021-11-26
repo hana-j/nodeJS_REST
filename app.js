f47c4c24660d1577c30d59d487fb5d67fed011c8
hana <aswjdgksk@gmail.con>
2021-11-26 22:50:37 +0900

    댓글수정삭제

--- app.js
+++ app.js
@@ -2,47 +2,49 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const methodOverride = require('method-override');
-const flash = require('connect-flash');//일회성 메세지를 웹 브라우저에 나타낼때 보통 사용
+const flash = require('connect-flash'); //일회성 메세지를 웹 브라우저에 나타낼때 보통 사용
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const bodyPaser = require('body-parser');
 const Post = require('./models/Post');
-const config = require('./config/key')
+const config = require('./config/key');
 const app = express();
 //DB
 //mongoose.connect("mongodb://localhost:27017/board1");
 mongoose.connect(config.mongoURI);
 
 const db = mongoose.connection;
-db.once('open', function(){
+db.once('open', function () {
     console.log('DB Connected');
 });
-db.on('error', function(err){
+db.on('error', function (err) {
     console.log('DB ERROR : ', err);
 });
 
 //미들웨어 설정들
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyPaser.json());
-app.use(bodyPaser.urlencoded({extended:true}));
+app.use(bodyPaser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(session({
-    secret:'hana',
-    resave:true,
-    saveUninitialized:true,
-}));
+app.use(
+    session({
+        secret: 'hana',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated(); //res.locals에 담겨진 변수는 ejs파일에서 바로 사용가능, 로그인했는지 안했는지 확인
-    res.locals.currentUser = req.user;   //로그인한 유저 확인
+    res.locals.currentUser = req.user; //로그인한 유저 확인
     next();
-})
+});
 
 //Routes ( 라우팅 미들웨어 )
 app.use('/', require('./routes'));
@@ -50,11 +52,8 @@ app.use('/posts', require('./routes/posts'));
 app.use('/users', require('./routes/users'));
 app.use('/comments', require('./routes/comment'));
 
-
-
 //Port 연결s
 const port = 3000;
-app.listen(port, function(){
+app.listen(port, function() {
     console.log('server on 3000');
 });
-
--- routes/users.js
+++ routes/users.js
@@ -9,69 +9,67 @@ const util = require('../util');
 //authToken
 router.post('/auth/tokens', AuthToken.create);
 //회원가입페이지
-router.get('/users', (req, res)=>{
-    const user= req.flash('user')[0] || {};
+router.get('/users', (req, res) => {
+    const user = req.flash('user')[0] || {};
     const errors = req.flash('errors')[0] || {};
-    res.render('users/join', {user:user, errors:errors});
-})
+    res.render('users/join', { user: user, errors: errors });
+});
 //회원가입
-router.post('/join',  (req,res)=>{
+router.post('/join', (req, res) => {
     console.log(req.body.nickname, req.body.password);
-    User.create(req.body, function(err, user){
-        if(err){
-            req.flash('user',req.body);
-            req.flash('errors', util.parseError(err));   //error 형식 맞춰주는 함수 이용 => 따로 공부하기 
-            return res.redirect('/users/users')
+    User.create(req.body, function (err, user) {
+        if (err) {
+            req.flash('user', req.body);
+            req.flash('errors', util.parseError(err)); //error 형식 맞춰주는 함수 이용 => 따로 공부하기
+            return res.redirect('/users/users');
         }
-        res.redirect('/users/login'); //성공시 로그인 페이지로 
+        res.redirect('/users/login'); //성공시 로그인 페이지로
     });
 });
 // 로그인페이지
-router.get('/login', function (req,res) {
+router.get('/login', function (req, res) {
     const nickname = req.flash('nickname')[0];
     const errors = req.flash('errors')[0] || {};
     res.render('users/login', {
-      nickname:nickname,
-      errors:errors
+        nickname: nickname,
+        errors: errors,
     });
-  });
+});
 
 //로그인 로직  => passport-jwt 사용으로 변경하기
-router.post('/auth', 
-    function(req,res,next){
-      var errors = {};
-      var isValid = true;
-  
-      if(!req.body.nickname){
-        isValid = false;
-        errors.username = 'Username is required!';
-      }
-      if(!req.body.password){
-        isValid = false;
-        errors.password = 'Password is required!';
-      }
-  
-      if(isValid){
-        next();
-      }
-      else {
-        req.flash('errors',errors);
-        res.redirect('/users/login');
-      }
+router.post(
+    '/auth',
+    function (req, res, next) {
+        var errors = {};
+        var isValid = true;
+
+        if (!req.body.nickname) {
+            isValid = false;
+            errors.username = 'Username is required!';
+        }
+        if (!req.body.password) {
+            isValid = false;
+            errors.password = 'Password is required!';
+        }
+
+        if (isValid) {
+            next();
+        } else {
+            req.flash('errors', errors);
+            res.redirect('/users/login');
+        }
     },
-    passport.authenticate('local', { //여기에서 localStrategy가 있는 파일을 찾아감 => config/passport.js
-      successRedirect : '/posts',
-      failureRedirect : '/users/login'
-    }
-  ));
-  //로그아웃
-  router.get('/logout',function(req, res) {
-    console.log(req.user);
+    passport.authenticate('local', {
+        //여기에서 localStrategy가 있는 파일을 찾아감 => config/passport.js
+        successRedirect: '/posts',
+        failureRedirect: '/users/login',
+    })
+);
+//로그아웃
+router.get('/logout', function (req, res) {
     req.logout();
     req.session.destroy();
     res.redirect('/posts');
-  });
-
-
+});
 
 module.exports = router;
